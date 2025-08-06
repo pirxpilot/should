e93b348837cd3062e0af164fbd50801a5a74c4ea
Damian Krzeminski <pirxpilot@furkot.com>
2025-08-06 15:10:55 -0600

    add named exports
    in addition to default `should` exports we are now also exporting
    `should`, `Assertion`, `AssertionError` and `format`

--- test/exports.test.js
+++ test/exports.test.js
@@ -1,12 +1,18 @@
-import assert from 'node:assert';
 import test from 'node:test';
-import should from '../lib/index.js';
+import shouldGlobal, { Assertion, AssertionError, format, should } from '../lib/index.js';
 
-test('should global', () => {
-  assert(typeof global.should === 'function');
+test('should global', t => {
+  t.assert.equal(typeof global.should, 'function');
 });
 
-test('should default export', () => {
-  assert(typeof should === 'function');
-  assert(typeof should.Assertion === 'function');
+test('should default export', t => {
+  t.assert.equal(typeof shouldGlobal, 'function');
+  t.assert.equal(typeof shouldGlobal.Assertion, 'function');
+});
+
+test('should named export', t => {
+  t.assert.equal(typeof should, 'function');
+  t.assert.equal(typeof Assertion, 'function');
+  t.assert.equal(typeof AssertionError, 'function');
+  t.assert.equal(typeof format, 'function');
 });
